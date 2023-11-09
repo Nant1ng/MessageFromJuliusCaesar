719d7954e6c4fe2e1e05865ea3a814ad36ff9d13
Nant1ng <andressantana99@hotmail.se>
2023-11-09 10:53:40 +0100

    Added a Menu

---
+++ MFJC/App.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFJC
+{
+    public class App
+    {
+        public static void Run()
+        {
+            bool exitMenu = false;
+
+            while (!exitMenu)
+            {
+                Console.WriteLine("1. Encrypted a Message.");
+                Console.WriteLine("2. Decrypted a Message.");
+                Console.WriteLine("3. Would you like to play guessing game? ");
+                Console.WriteLine("0. Exit.");
+
+                string? choice = Console.ReadLine();
+
+                if (int.TryParse(choice, out int menuChoice))
+                {
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            Encrypter.Encryption();
+                            break;
+                        case 2:
+                            Decrypter.Decryption();
+                            break;
+                        case 3:
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Invalid choice. Please enter a valid menu option. 1, 2 or 0.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid integer menu option. 1, 2 or 0.");
+
+                }
+            }
+        }
+    }
+}
--- MFJC/Program.cs
+++ MFJC/Program.cs
@@ -4,26 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var myEncrypter = new CaeserCipher();
-
-            Console.Write("Skriv din text du vill kryptera: ");
-            string? inputValue = Console.ReadLine();
-
-            if (!string.IsNullOrWhiteSpace(inputValue))
-            {
-                Console.Write("En nyckel mellan 1-26: ");
-                bool intCheck = int.TryParse(Console.ReadLine(), out int value);
-
-                if (intCheck && value > 0 && value < 27)
-                {
-                    string encryption = myEncrypter.EncryptedMessage(inputValue, value);
-                    string decryption = myEncrypter.DecryptedMessage(encryption, value);
-
-                    Console.WriteLine($"{encryption}");
-                    Console.WriteLine($"{decryption}");
-
-                }
-            }
+            App.Run();
         }
     }
 }
