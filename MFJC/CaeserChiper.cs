59a6310b6e9c577a6ee0021efa3dc3c0161df741
Emil Danielsson <emil.danielsson2@learnet.se>
2023-11-09 09:32:16 +0100

    added class Chiper encryption works

---
+++ MFJC/CaeserChiper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFJC
+{
+    public class CaeserChiper
+    {
+        public string? _EncryptedMessage { get; set; }
+        public string EncrpytedMessage(string text, int key)
+        {
+            string encryptedText = "";
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    encryptedText += (char)(((c + key - offset) % 26) + offset);
+                }
+                else
+                {
+                    encryptedText += c;
+                }
+            }
+            _EncryptedMessage = encryptedText;
+            return _EncryptedMessage;
+        }
+    }
+}
--- MFJC/Program.cs
+++ MFJC/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            var myEncrypter = new CaeserChiper();
+            Console.Write("Skriv din text du vill kryptera: ");
+            string? inputValue = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(inputValue))
+            {
+                Console.Write("En nyckel mellan 1-26: ");
+                bool intCheck = int.TryParse(Console.ReadLine(), out int value);
+                if (intCheck && value > 0 && value < 27)
+                {
+                    myEncrypter.EncrpytedMessage(inputValue, value);
+                    Console.WriteLine(myEncrypter._EncryptedMessage);
+                }
+            }
         }
     }
 }
